befd639fff46088d57998423f25c538dee930725
Bunchango <119033041+Bunchango@users.noreply.github.com>
2023-09-22 16:12:15 +0700

    remove useless code

--- TerminalPortManagementSystem/Main.java
+++ TerminalPortManagementSystem/Main.java
@@ -1,8 +1,6 @@
 package TerminalPortManagementSystem;
 
 import TerminalPortManagementSystem.Interface.InterfaceSystem;
-import TerminalPortManagementSystem.Utility.Prettify;
-import TerminalPortManagementSystem.Utility.TerminalUtil;
 
 public class Main {
     public static void main(String[] args) {
